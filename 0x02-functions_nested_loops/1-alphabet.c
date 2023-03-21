4a7e26869f5e44e3a9f7237bfb3a17052706845e
Siyasanga98 <siyasangavilakati98@gmail.com>
2023-03-21 14:07:29 -0700

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints the alphabet lowecase
- * 
- * Return: Always nothing 
+ *
+ * Return: Always nothing
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}

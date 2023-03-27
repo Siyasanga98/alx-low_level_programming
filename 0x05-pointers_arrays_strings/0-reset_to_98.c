f8762ae5baa5d142dd2b1160652c96bb836719d4
Siyasanga98 <siyasangavilakati98@gmail.com>
2023-03-27 09:43:18 -0700

    task o

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * reset_to_98 - update the value of n to 98
- * @n: input
- * Return: n
+ *
+ * @n:an interger input pointer
+ *
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {

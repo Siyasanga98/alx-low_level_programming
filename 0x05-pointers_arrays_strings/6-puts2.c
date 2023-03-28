686c5c82455318de70e2ff13734caced0b0f9556
Siyasanga98 <siyasangavilakati98@gmail.com>
2023-03-27 18:55:17 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Print every other character of a string
+ * @str: The steing to be treated
+ * Return: void
+ */
+void puts2(char*str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	for (i = 0; i < j; 1 += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}

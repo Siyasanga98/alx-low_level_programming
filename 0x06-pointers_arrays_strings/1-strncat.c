c146d1a51dc40281f11bdb8ec592cd0e79469500
Siyasanga98 <siyasangavilakati98@gmail.com>
2023-03-30 11:39:37 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,11 +5,10 @@
  * using at most n bytes from src
  * @dest: input value
  * @src: input value
- * @n: input value
- *
+ * @n: input value *
  * Return: dest
  */
-char *_strcat(char *dest, char *src,int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;

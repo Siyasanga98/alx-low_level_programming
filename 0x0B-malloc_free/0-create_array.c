fe5db256efa23069afaafcb239eb3152a028408b
Siyasanga98 <siyasangavilakati98@gmail.com>
2023-04-12 07:09:51 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,14 +3,14 @@
 /**
  * create_array - function creates array of size and assign char
  * @c : to assign
- * Return: NULL if fail 
+ * Return: NULL if fail
  */
 char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
 
-	str = malloc(sizeof(char) *size);
+	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 		return (NULL);
 

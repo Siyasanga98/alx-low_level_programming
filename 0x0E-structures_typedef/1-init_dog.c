3569996dec6994a2c7dc1c7b5e2c4e4ec6c86316
Siyasanga98 <siyasangavilakati98@gmail.com>
2023-04-18 14:49:00 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -20,3 +20,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 }
+int main() 
+{
+	struct dog my_dog;
+	init_dog(&my_dog, "Fido", 3.5, "Betty");printf("My dog's name is %s, age is %.1f, and owner is %s.\n", my_dog.name, my_dog.age, my_dog.owner);
+	return 0;
+}
+

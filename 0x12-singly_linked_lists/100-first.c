76e9c5b803d22f916bae0eaef2839dd1952841eb
siyasanga98 <siyasangavilakati98@gmail.com>
2023-04-28 15:24:12 -0700

    task 5 advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+* first - ts executed before main
+*Return: no return
+*/
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
+
+int main(void)
+{
+	return (0);
+}

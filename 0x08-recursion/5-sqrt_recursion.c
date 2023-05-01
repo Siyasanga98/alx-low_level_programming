53f6822d7491e056fd349eb36b4ecf57d63d684d
siyasanga98 <siyasangavilakati98@gmail.com>
2023-05-01 08:10:22 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,31 +5,31 @@ int _sqrt(int n, int i);
 
 /**
  * _sqrt_recursion - returns square root of a number
- * @n: the number to calculate square root 
+ * @n: the number to calculate square root
  *
  * Return: the natural square root o
  */
 int _sqrt_recursion(int n)
 {
-        return (_sqrt(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
  * _sqrt - calculate the square root of a number
- * @n: number used to find square root 
+ * @n: number used to find square root
  * @i: repeats number
  *
  * Return: the square root ,or -1
  */
 int _sqrt(int n, int i)
 {
-        int square = i * i;
+	int square = i * i;
 
-        if (square > n)
-                return (-1);
-        else if (square == n)
-                return (i);
-        else
-                return (_sqrt(n, i + 1));
+	if (square > n)
+		return (-1);
+	else if (square == n)
+		return (i);
+	else
+		return (_sqrt(n, i + 1));
 }
 

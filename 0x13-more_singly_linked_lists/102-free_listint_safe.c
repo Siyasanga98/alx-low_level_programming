28454fbb076ce2f0cc573f50ec43566d92e6c6f0
siyasanga98 <siyasangavilakati98@gmail.com>
2023-05-03 06:43:13 -0700

    advanced task 3

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,31 +2,31 @@
 
 /**
  * free_listint_safe - frees a listint_t list safely
- * @h: pointer to pointer of the head of the list
+ * @h: pointer to first head of the list
  *
- * Return: the size of the list that was free'd
+ * Return: the size of the list
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *current, *next;
-    size_t count = 0;
+	listint_t *current, *next;
+	size_t count = 0;
 
-    if (h == NULL || *h == NULL)
+	if (h == NULL || *h == NULL)
         return (0);
 
-    current = *h;
-    *h = NULL; /* set head to NULL before freeing */
+	current = *h;
+	*h = NULL;
 
-    while (current != NULL)
-    {
-        count++;
-        next = current->next;
-        free(current);
-        if (next >= current)
-            break;
-        current = next;
-    }
+	while (current != NULL)
+	{
+		count++;
+		next = current->next;
+		free(current);
+		if (next >= current)
+			break;
+		current = next;
+	}
 
-    return (count);
+	return (count);
 }

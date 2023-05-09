e6cf0a49b1980c1d9565232296600b38d1db2119
siyasanga98 <siyasangavilakati98@gmail.com>
2023-05-08 19:35:23 -0700

    advanced task

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,40 @@
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <elf.h>
+
+void print_elf_header(const char* filename);
+void print_magic(const unsigned char* magic);
+void print_class(unsigned char class);
+void print_data(unsigned char data);
+void print_version(unsigned char version);
+void print_abi(unsigned char abi);
+void print_osabi(unsigned char osabi);
+void print_type(unsigned short type);
+void print_entry(Elf64_Addr entry);
+void close_elf (int elf);
+/**
+ * check_elf - checks the file
+ * @e_ident: arrary conyaning the elf magic number
+ *
+ * Description: if a dile s not the elf file exit
+ */
+void check_elfi(unsigned char *e_ident)
+{
+	int index;
+
+	for(index = 0; index < 4; index++)
+	{
+		if (e_ident[index] != 127 &&
+		e_ident[index] != 'E' &&
+		e_ident[index] != 'L' &&
+		e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: not an ELF file\n");
+			exit(98);
+		}
+	}
+}

5d9d02b8aa9359c15447a734be2748135c96a241
siyasanga98 <siyasangavilakati98@gmail.com>
2023-05-11 04:04:43 -0700

    advanced task bit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks if a machine is little or big
+ *
+ * Return: 0 if big , 1 if little
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *byte = (char *)&num;
+
+	return (int)*byte;
+}
